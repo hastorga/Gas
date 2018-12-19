c50c1482cc06fcddb07131fd6a3cd21c47c35f04
Héctor <hm.astorga@gmail.com>
2018-12-19 00:00:55 -0300

    Feat: clase abstracta Persona
    Tendrá herencia hacia Trabajador y Cliente

--- src/Clases/Persona.java
+++ src/Clases/Persona.java
@@ -1,5 +1,62 @@
 package Clases;
 
 public abstract class Persona {
+	
+	private String nombre, rut, direccion;
+	private int telefono;
+	
+	/*********************** CONSTRUCTOR *****************************/
+	public Persona (String rut, String nombre, String direccion, int telefono)
+	{
+		this.rut = rut;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.telefono = telefono;
+	}
+	
+	/********************* SETTERS AND GETTERS ***********************/
+	public void set_rut (String rut)
+	{
+		
+		this.rut = rut;
+	}
+	
+	public String get_rut ()
+	{
+		return rut;
+	}
+	public void set_nombre (String nombre)
+	{
+		this.nombre = nombre;
+		
+	}
+	public String get_nombre ()
+	{
+		return nombre;
+	}
+	
+	public String get_direccion ()
+	{
+		return direccion;
+	}
+	
+	public void set_direccion(String direccion)
+	{
+		this.direccion = direccion;
+	}
+	
+	public int get_telefono ()
+	{
+		return telefono;
+	}
+	public void set_telefono(int telefono)
+	{
+		this.telefono = telefono; 
+	}
+	
+	
+
+
+	
 
 }
