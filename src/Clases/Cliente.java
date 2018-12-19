4140bccb617e031081346a504389c10f1b7fa4a6
Héctor <hm.astorga@gmail.com>
2018-12-19 00:01:20 -0300

    Feat: Cliente y Trabajador
    Por el momento se agregan solo Setters and Getters.

--- src/Clases/Cliente.java
+++ src/Clases/Cliente.java
@@ -1,9 +1,18 @@
 package Clases;
 
-public class Cliente {
+public class Cliente extends Persona{
 
-	public Cliente() {
-		// TODO Auto-generated constructor stub
+	private String ref_direccion;
+	public Cliente(String nombre, String rut, String direccion, String ref_direccion, int telefono) {
+		super(nombre, rut, direccion, telefono);
+		this.ref_direccion = ref_direccion;
+	}
+	
+	public void set_referencia(String referencia) {
+		this.ref_direccion = referencia;
+	}
+	public String get_referencia() {
+		return ref_direccion;
 	}
 
 }
--- src/Clases/Trabajador.java
+++ src/Clases/Trabajador.java
@@ -1,9 +1,35 @@
 package Clases;
 
-public class Trabajador {
+public class Trabajador extends Persona {
+	private String sector, patente_camioneta;
 
-	public Trabajador() {
-		// TODO Auto-generated constructor stub
+	public Trabajador(String nombre, String rut, String direccion, int telefono, String sector, String patente) {
+		
+		super(nombre, rut, direccion, telefono);
+		this.sector = sector;
+		this.patente_camioneta = patente;
+		
 	}
+	
+	public String get_sector()
+	{
+		return sector;
+	}
+	public void set_sector (String sector)
+	{
+		this.sector = sector;
+		
+	}
+	public String get_patente ()
+	{
+		return patente_camioneta;
+	}
+	
+	public void set_patente (String patente)
+	{
+		this.patente_camioneta = patente;
+	}
+	
+
 
 }
