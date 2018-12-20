30f946b096165e061fed9aaa46146060bdaeec98
Héctor <hm.astorga@gmail.com>
2018-12-20 01:01:41 -0300

    Fix: orden de atributos de constructor corregido

--- src/Clases/Cliente.java
+++ src/Clases/Cliente.java
@@ -3,9 +3,9 @@ package Clases;
 public class Cliente extends Persona{
 
 	private String ref_direccion;
-	public Cliente(String nombre, String rut, String direccion, String ref_direccion, int telefono) {
-		super(nombre, rut, direccion, telefono);
-		this.ref_direccion = ref_direccion;
+	public Cliente(String rut,String nombre, String direccion,String comuna, int telefono) {
+		super(nombre, rut, direccion, telefono, comuna);
+		this.ref_direccion = "opcional";
 	}
 	
 	public void set_referencia(String referencia) {
